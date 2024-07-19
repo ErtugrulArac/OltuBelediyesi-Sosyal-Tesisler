204252d0909d73804b0526392db43abb357f1e15
ErtugrulArae <aracertugrul407@gmail.com>
2024-07-19 14:05:04 +0300

    resim

--- components/question/index.tsx
+++ components/question/index.tsx
@@ -5,7 +5,7 @@ import Faq from "@/components/question/faq";
 
 export default function Home() {
   return (
-    <div className="bg-white py-28">
+    <div className="bg-white py-28 max-md:py-14">
       <div className="flex w-[90%] max-xl:w-[90%] m-auto justify-center items-center gap-12 max-lg:flex-col ">
         <div className="flex justify-center items-center flex-col max-xl:flex-1">
           <img className="w-[30%] max-md:w-[60%]" src="soru.png" alt="qr menü soru cevap" />
